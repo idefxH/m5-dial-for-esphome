21733d800ae8a93aa3a8fc9203475654af36f79d
Ernesto Martinez <ernesto.e.martinez@gmail.com>
2024-11-17 13:49:37 +0800

    Update ha_device_mode_light_on_off.h

--- components/shys_m5_dial/ha_device_mode_light_on_off.h
+++ components/shys_m5_dial/ha_device_mode_light_on_off.h
@@ -22,12 +22,12 @@ namespace esphome
                     uint16_t height = gfx->height();
                     uint16_t width  = gfx->width();
 
-                    gfx->setTextColor(SHOWONOFFMENUTEXT); //MAROON
+                    gfx->setTextColor(SHOWONOFFMENUTEXTCOLOR); //MAROON
                     gfx->setTextDatum(middle_center);
 
                     gfx->startWrite();                      // Secure SPI bus
 
-                    gfx->fillRect(0, 0, width, height, currentValue>0?SHOWONOFFMENUBACK_ON:SHOWONOFFMENUBACK_OFF); // YELLOW:RED
+                    gfx->fillRect(0, 0, width, height, currentValue>0?SHOWONOFFMENUBACKCOLOR_ON:SHOWONOFFMENUBACKCOLOR_OFF); // YELLOW:RED
 
                     display.setFontsize(3);
                     gfx->drawString(currentValue>0?"on":"off",
