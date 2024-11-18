1d3a2aebce15f649bbb33f8a20c903ca59030e49
Ernesto Martinez <ernesto.e.martinez@gmail.com>
2024-11-18 21:42:48 +0800

    Update m5dial_display_colors.h
    Update colour definitions

--- components/shys_m5_dial/m5dial_display_colors.h
+++ components/shys_m5_dial/m5dial_display_colors.h
@@ -2,71 +2,113 @@
 // Temperature Menu
 #ifndef TemperatureMenuTextColor
   #define TemperatureMenuTextColor      BLACK
+#endif
+#ifndef TemperatureMenuBackColorOff
   #define TemperatureMenuBackColorOff   RED
+#endif
+#ifndef TemperatureMenuBackColorOn
   #define TemperatureMenuBackColorOn    PINK
 #endif
 
 // Fan Menu
 #ifndef FanMenuTextColor
   #define FanMenuTextColor              BLACK
+#endif
+#ifndef FanMenuBackColorOff
   #define FanMenuBackColorOff           DARKGREEN
+#endif
+#ifndef FanMenuBackColorOn
   #define FanMenuBackColorOn            GREEN
 #endif
 
 // Light Brightness Menu
 #ifndef BrightnessMenuTextColor
   #define BrightnessMenuTextColor       BLACK
+#endif
+#ifndef BrightnessMenuBackColorOff
   #define BrightnessMenuBackColorOff    DARKCYAN
+#endif
+#ifndef BrightnessMenuBackColorOn
   #define BrightnessMenuBackColorOn     CYAN
 #endif
 
 // On/Off Light Menu
 #ifndef OnOffMenuTextColor
   #define OnOffMenuTextColor            BLACK
+#endif
+#ifndef OnOffMenuBackColorOff
   #define OnOffMenuBackColorOff         BROWN
+#endif
+#ifndef OnOffMenuBackColorOn
   #define OnOffMenuBackColorOn          YELLOW
 #endif
 
 // Tunable White Light Menu
 #ifndef TunableWhiteMenuTextColor
   #define TunableWhiteMenuTextColor     BLACK
+#endif
+#ifndef TunableWhiteMenuBackColorOff
   #define TunableWhiteMenuBackColorOff  NAVY
+#endif
+#ifndef TunableWhiteMenuBackColorOn
   #define TunableWhiteMenuBackColorOn   BLUE
 #endif
 
 // Lock Menu
 #ifndef LockStatusMenuTextColor
   #define LockStatusMenuTextColor       BLACK
+#endif
+#ifndef LockStatusMenuBackColorOff
   #define LockStatusMenuBackColorOff    DARKGREEN
+#endif
+#ifndef LockStatusMenuBackColorOn
   #define LockStatusMenuBackColorOn     GREEN
 #endif
 
 // Media Player Menu
 #ifndef PlayMenuTextColor
   #define PlayMenuTextColor             BLACK
+#endif
+#ifndef PlayMenuBackColorOff
   #define PlayMenuBackColorOff          GOLD
+#endif
+#ifndef PlayMenuBackColorOn
   #define PlayMenuBackColorOn           YELLOW
+#endif
+#ifndef PlayMenuBackColorTran
   #define PlayMenuBackColorTran         ORANGE
 #endif
 
 // Media Player Source Menu
 #ifndef SourceSelectionTextColor
   #define SourceSelectionTextColor      BLACK
+#endif
+#ifndef SourceSelectionBackColorOff
   #define SourceSelectionBackColorOff   GOLD
+#endif
+#ifndef SourceSelectionBackColorOn
   #define SourceSelectionBackColorOn    YELLOW
 #endif
 
 // Percentage Menu
 #ifndef PercentageMenuTextColor
   #define PercentageMenuTextColor       BLACK
+#endif
+#ifndef PercentageMenuBackColorOff
   #define PercentageMenuBackColorOff    PURPLE
+#endif
+#ifndef PercentageMenuBackColorOn
   #define PercentageMenuBackColorOn     VIOLET
 #endif
 
 // On/Off Menu
 #ifndef OnOffMenuTextColor
   #define OnOffMenuTextColor            BLACK
+#endif
+#ifndef OnOffMenuBackColorOff
   #define OnOffMenuBackColorOff         NAVY
+#endif
+#ifndef OnOffMenuBackColorOn
   #define OnOffMenuBackColorOn          BLUE
 #endif
 
